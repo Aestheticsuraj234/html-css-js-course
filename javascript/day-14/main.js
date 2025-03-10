a8f5ec2b80147c39ff0cce42733bd133df5bcdaf
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-03-10 11:57:05 +0530

    day-14

---
+++ javascript/day-14/main.js
@@ -0,0 +1,32 @@
+// let , var ,const
+
+// keyword variableName = value;
+
+
+// var username = "Jhon";
+// // var username = "Suraj"
+
+// let age = 20;
+// const pi = 3.14;
+
+
+let username = "Suraj"
+let age = 20;
+let isProgrammer = true;
+let myUndefined;
+let myNull = null;
+
+
+let myArr = ["Suraj", "Jhon", "Saransh"]
+
+let myObj = {
+    name: "Suraj",
+    age: 20,
+    isProgrammer: true
+
+}
+
+
+function add(){
+    
+}
